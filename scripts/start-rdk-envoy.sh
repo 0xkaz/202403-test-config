ce4e59192d5923aa44a52f441e7e7feb2051a64d
kaz <kzzz123@protonmail.com>
2024-03-20 13:30:29 +0800

    to suport rdk

---
+++ scripts/start-rdk-envoy.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+set -e
+
+# LISTEN_PORT_HTTPS=${LISTEN_PORT_HTTPS:-8080}
+# LISTEN_PORT_HTTP=${LISTEN_PORT_HTTP:-8080}
+LISTEN_PORT_HTTPS=${LISTEN_PORT_HTTPS:-443}
+LISTEN_PORT_HTTP=${LISTEN_PORT_HTTP:-80}
+LISTEN_PORT=${LISTEN_PORT:-8080}
+ADMIN_LISTEN_PORT=${ADMIN_LISTEN_PORT:-9901}
+SERVICE_DISCOVERY_ADDRESS=${SERVICE_DISCOVERY_ADDRESS:-0.0.0.0}
+SERVICE_DISCOVERY_PORT=${SERVICE_DISCOVERY_PORT:-9090}
+HTTPS_DOMAIN=${HTTPS_DOMAIN:-test.raas.weavedb-node.xyz}
+
+# envoy.https.tmpl.yaml
+
+# envoy.tmpl.yaml
+
+
+cat /rdk/node/envoy/envoy.https.tmpl.yaml | \
+/bin/sed -e "s/\$LISTEN_PORT_HTTPS/$LISTEN_PORT_HTTPS/g" | \
+/bin/sed -e "s/\$LISTEN_PORT_HTTP/$LISTEN_PORT_HTTP/g" | \
+/bin/sed -e "s/\$ADMIN_LISTEN_PORT/$ADMIN_LISTEN_PORT/g" | \
+/bin/sed -e "s/\$LISTEN_PORT/$LISTEN_PORT/g" | \
+/bin/sed -e "s/\$SERVICE_DISCOVERY_PORT/$SERVICE_DISCOVERY_PORT/g" | \
+/bin/sed -e "s/\$HTTPS_DOMAIN/$HTTPS_DOMAIN/g" | \
+/bin/sed -e "s/\$SERVICE_DISCOVERY_ADDRESS/$SERVICE_DISCOVERY_ADDRESS/g" \
+> /rdk/node/envoy/envoy.yaml
+
+cat /rdk/node/envoy/envoy.yaml
+
+/usr/local/bin/envoy -c /rdk/node/envoy/envoy.yaml
