9e2b862ac39d7329792e630b216d9c1740842cac
xverse <kzzz123@protonmail.com>
2024-03-26 17:31:27 +0800

    fix

--- conf/weavedb.standalone.config.tmpl.js
+++ conf/weavedb.standalone.config.tmpl.js
@@ -2,11 +2,11 @@ module.exports = {
   admin: "$ADMIN_PRIVATE_KEY",
   bundler:
     {"kty":"$BUNDLER_KTY",
-    "n":"$BUNDLER_N",
-    "e":"$BUNDLER_E",
-    "d":"$BUNDLER_D",
-    "p":"$BUNDLER_P",
-    "q":"$BUNDLER_Q",
+    "n":"$BUNDLER__N",
+    "e":"$BUNDLER__E",
+    "d":"$BUNDLER__D",
+    "p":"$BUNDLER__P",
+    "q":"$BUNDLER__Q",
     "dp":"$BUNDLER_DP",
     "dq":"$BUNDLER_DQ",
     "qi":"$BUNDLER_QI"
--- scripts/setup-rdk-config.sh
+++ scripts/setup-rdk-config.sh
@@ -6,11 +6,11 @@ set +a
 
 ADMIN_PRIVATE_KEY=${ADMIN_PRIVATE_KEY}
 BUNDLER_KTY=${BUNDLER_KTY:-RSA}
-BUNDLER_N=${BUNDLER_N}
-BUNDLER_E=${BUNDLER_E}
-BUNDLER_D=${BUNDLER_D}
-BUNDLER_P=${BUNDLER_P}
-BUNDLER_Q=${BUNDLER_Q}
+BUNDLER__N=${BUNDLER__N}
+BUNDLER__E=${BUNDLER__E}
+BUNDLER__D=${BUNDLER__D}
+BUNDLER__P=${BUNDLER__P}
+BUNDLER__Q=${BUNDLER__Q}
 BUNDLER_DP=${BUNDLER_DP}
 BUNDLER_DQ=${BUNDLER_DQ}
 BUNDLER_QI=${BUNDLER_QI}
@@ -18,11 +18,11 @@ BUNDLER_QI=${BUNDLER_QI}
 cat /rdk/node/node-server/weavedb.config.tmpl.js | \
 /bin/sed -e "s/\$ADMIN_PRIVATE_KEY/$ADMIN_PRIVATE_KEY/g" |  \
 /bin/sed -e "s/\$BUNDLER_KTY/$BUNDLER_KTY/g" |  \
-/bin/sed -e "s/\$BUNDLER_N/$BUNDLER_N/g" | \
-/bin/sed -e "s/\$BUNDLER_E/$BUNDLER_E/g" | \
-/bin/sed -e "s/\$BUNDLER_D/$BUNDLER_D/g" | \
-/bin/sed -e "s/\$BUNDLER_P/$BUNDLER_P/g" | \
-/bin/sed -e "s/\$BUNDLER_Q/$BUNDLER_Q/g" | \
+/bin/sed -e "s/\$BUNDLER__N/$BUNDLER__N/g" | \
+/bin/sed -e "s/\$BUNDLER__E/$BUNDLER__E/g" | \
+/bin/sed -e "s/\$BUNDLER__D/$BUNDLER__D/g" | \
+/bin/sed -e "s/\$BUNDLER__P/$BUNDLER__P/g" | \
+/bin/sed -e "s/\$BUNDLER__Q/$BUNDLER__Q/g" | \
 /bin/sed -e "s/\$BUNDLER_DP/$BUNDLER_DP/g" | \
 /bin/sed -e "s/\$BUNDLER_DQ/$BUNDLER_DQ/g" | \
 /bin/sed -e "s/\$BUNDLER_QI/$BUNDLER_QI/g" > /tmp/weavedb.config.js
